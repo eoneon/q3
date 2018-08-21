9824127d1e8e8d2cd1738aaf091d445e10dd2d5a
Eon <eoneon123@gmail.com>
2018-08-21 10:52:32 -0700

    added set_sort and reset_sort methods for field_groups

--- app/controllers/field_groups_controller.rb
+++ app/controllers/field_groups_controller.rb
@@ -1,9 +1,10 @@
 class FieldGroupsController < ApplicationController
   def create
     @category = Category.find(params[:category_id])
-    @field_group = @category.field_groups.build(field_group_params)
+    field_group = @category.field_groups.build(field_group_params)
+    set_sort(@category, field_group)
 
-    if @field_group.save
+    if field_group.save
       flash[:notice] = "Field chain was saved successfully."
       redirect_to @category
     else
@@ -17,10 +18,11 @@ class FieldGroupsController < ApplicationController
 
   def destroy
     @category = Category.find(params[:category_id])
-    field = Field.find(params[:id])
     field_group = @category.field_groups.find_by(field_id: params[:id])
+    sort = field_group.sort
 
     if field_group.destroy
+      reset_sort(@category, sort)
       flash[:notice] = "Field chain was deleted successfully."
       redirect_to @category
     else
@@ -33,5 +35,23 @@ class FieldGroupsController < ApplicationController
 
   def field_group_params
     params.require(:field_group).permit!
+  end
+
+  def siblings(category)
+    category.fields
+  end
+
+  def max_sort(category)
+    siblings(category).count
+  end
+
+  def set_sort(category, field_group)
+    field_group.sort = max_sort(category) == 0 ? 1 : max_sort(category) + 1
+  end
+
+  def reset_sort(category, sort)
+    category.field_groups.where("sort > ?", sort).each do |field|
+      field.update(sort: field.sort - 1)
+    end
   end
 end
--- app/models/field_group.rb
+++ app/models/field_group.rb
@@ -1,4 +1,17 @@
 class FieldGroup < ApplicationRecord
   belongs_to :category
   belongs_to :field, optional: true
+
+  # before_save :set_sort
+
+  # def set_sort
+  # end
+  #scope :siblings, -> {where(category_id: self.category_id)}
+  # def self.siblings
+  #   FieldGroup.where(category_id: self.category_id)
+  # end
+
+  # def parent
+  #   category.fields
+  # end
 end
