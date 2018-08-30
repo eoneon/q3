c0ccc9ea1cc1bcb6b538f81dbb28bf863cc40d07
Eon <eoneon123@gmail.com>
2018-08-29 17:49:03 -0700

    added dynamic methods to application_records that build links based on whether an hmt obj is nested or not

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,8 +1,27 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  def req_meth(req_path, params)
-    obj_path = req_path[req_path.index(params[:controller])-1..req_path.index(params[:id])]
-    req_path.partition(obj_path)
+  def params_arr(req_path, params)
+    [:controller, :id].map {|k| "/#{params[k]}"}
+  end
+
+  def partitioned_path(req_path, params)
+    req_path.partition(params_arr(req_path, params)[0])
+  end
+
+  def nested_path?(req_path, params)
+    partitioned_path(req_path, params)[0].present?
+  end
+
+  def dynamic_index(req_path, params)
+    nested_path?(req_path, params) ? partitioned_path(req_path, params)[0] : params_arr(req_path, params)[0]
+  end
+
+  def dynamic_show(req_path, params)
+    nested_path?(req_path, params) ? partitioned_path(req_path, params)[0] + params_arr(req_path, params).join("") : params_arr(req_path, params).join("")
+  end
+
+  def dynamic_edit(req_path, params)
+    dynamic_show(req_path, params) + "/edit"
   end
 end
