21d13e2dad2d0e8a59a7bdea5fee2be8254b2ff2
Eon <eoneon123@gmail.com>
2018-11-29 08:23:16 -0800

    refactored category-value views; part 1 before second rebuild

--- app/helpers/values_helper.rb
+++ app/helpers/values_helper.rb
@@ -1,2 +1,8 @@
 module ValuesHelper
+  # def radio_input_group(f, value)
+  #   %(title body attribute).each do |k|
+  #     [['radio_button', value.name, checked: true],['radio_button', 'none'],['radio_button', 'custom'],['text_field', :title_value]].each do |args|
+  #
+  #   end
+  # end
 end
--- app/models/value.rb
+++ app/models/value.rb
@@ -4,6 +4,26 @@ class Value < ApplicationRecord
   has_many :value_groups, dependent: :destroy
   has_many :items, through: :value_groups
 
+  before_create :set_properties
+  before_update :reset_properties
+
+  def set_properties
+    self.properties["title_value"] = name
+    self.properties["body_value"] = name
+    self.properties["attribute_value"] = name
+  end
+
+  def reset_properties
+    # if properties_changed?
+      %w(title body attribute).each do |k|
+        if properties[k] == 'none'
+          properties["#{k}_value"] = ""
+        elsif properties[k] == name
+          properties["#{k}_value"] = name
+        end
+      end
+    # end
+  end
   #before_create :set_sort
   #after_save :set_parent
 
@@ -16,6 +36,6 @@ class Value < ApplicationRecord
   #   Value.all.order(:sort)
   # end
   # def self.field_keys
-  #   %w[type parent title body both attribute selected display_hidden display_none]
+  #   %w[parent title body both attribute selected display_hidden display_none]
   # end
 end
